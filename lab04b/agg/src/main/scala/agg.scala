fa4ea85d8223e7867991ad5b00806f6a04e29c8c
alcherniaev <playtowin19@gmail.com>
2021-03-24 21:30:21 +0300

    kafka test

--- lab04b/agg/src/main/scala/agg.scala
+++ lab04b/agg/src/main/scala/agg.scala
@@ -1,25 +1,26 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType, TimestampType}
-import org.apache.spark.sql.functions.{col, from_json, lit, struct, sum, to_json, when, window}
+import org.apache.spark.sql.functions.{col, from_json, sum, when, window}
 import org.apache.spark.sql.streaming.Trigger
-
 
 object agg {
   def main(args: Array[String]) = {
     val spark = SparkSession
       .builder
-      .appName("lab04b")
+      .appName("Lab04")
       .getOrCreate()
 
     import spark.implicits._
 
-    val initialDF = spark.readStream
+    val dfInput = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "spark-master-1:6667")
       .option("subscribe", "alexey_chernyaev2")
+      //      .option("startingOffsets", "earliest")
+      //      .option("endingOffsets", "latest")
       .load()
 
-    val df = initialDF.select($"value".cast("string").as[String])
+    var df = dfInput.selectExpr("CAST(value AS STRING)")
 
     val schema = StructType(Seq(
       StructField("category", StringType, true),
@@ -30,23 +31,27 @@ object agg {
       StructField("uid", StringType, true)
     ))
 
-    val processedDF = df.withColumn("jsonData", from_json(col("value"), schema)).select("jsonData.*")
-                        .withColumn("date", ($"timestamp" / 1000).cast(TimestampType))
+    df = df.withColumn("jsonData", from_json(col("value"), schema)).select("jsonData.*")
 
-    val dfW = processedDF.groupBy(window(col("date"), "1 hours")).agg(
-      sum(when($"event_type" === "buy", col("item_price")).otherwise(0)).alias("revenue"),
-      sum(when($"uid".isNotNull, 1).otherwise(0)).alias("visitors"),
-      sum(when($"event_type" === "buy", 1).otherwise(0)).alias("purchases"))
-      .withColumn("aov", $"revenue" / $"purchases")
-      .withColumn("start_ts", $"window.start".cast("long"))
-      .withColumn("end_ts", $"window.end".cast("long")).drop(col("window"))
+    df = df.withColumn("date", ($"timestamp" / 1000).cast(TimestampType))
 
-    val query = dfW
+    df = df.groupBy(window(col("date"), "1 hours"/*, "5 seconds"*/)).agg(
+      sum(when(col("event_type") === "buy", col("item_price")).otherwise(0)).alias("revenue"),
+      sum(when(col("uid").isNotNull, 1).otherwise(0)).alias("visitors"),
+      sum(when(col("event_type") === "buy", 1).otherwise(0)).alias("purchases")
+    )
+
+    df = df.withColumn("aov", col("revenue")/col("purchases"))
+    df = df.withColumn("start_ts", col("window.start").cast("long"))
+    df = df.withColumn("end_ts", col("window.end").cast("long"))
+    df = df.drop(col("window"))
+
+    val query = df
       .selectExpr("CAST(start_ts AS STRING) AS key", "to_json(struct(*)) AS value")
       .writeStream
       .trigger(Trigger.ProcessingTime("5 seconds"))
       .format("kafka")
-      .option("checkpointLocation", "lab04b_checkpoint_alexey_chernyaev2")
+      .option("checkpointLocation", "lab04b_checkpointloc_alexey_chernyaev2")
       .option("kafka.bootstrap.servers", "spark-master-1:6667")
       .option("topic", "alexey_chernyaev2_lab04b_out")
       .option("maxOffsetsPerTrigger", 200)
@@ -55,27 +60,7 @@ object agg {
 
 
     query.awaitTermination()
-//    dfW
-//      .select($"start_ts".cast("string").alias("key"), to_json(struct("*")).alias("value"))
-//      .writeStream
-//      .format("kafka")
-//      .option("kafka.bootstrap.servers", "spark-master-1:6667")
-///      .option("topic", "alexey_chernyaev2_lab04b_out")
-//      .option("checkpointLocation", "/user/alexey.chernyaev2/lab04b_checkpoint")
-//      .outputMode("update")
-//      .trigger(Trigger.ProcessingTime("5 seconds"))
-//      .start()
 
-
-//    dfW
-//      .selectExpr("CAST(start_ts AS STRING) AS key", "to_json(struct(*)) AS value")
-//     .writeStream
-//      .format("kafka")
-//      .option("kafka.bootstrap.servers", "spark-master-1:6667")
-//      .option("topic", "alexey_chernyaev2_lab04b_out")
-//      .option("checkpointLocation", "lab04b_checkpoint_alexey_chernyaev2")
-//      .outputMode("update")
-//      .start()
 
 
   }
